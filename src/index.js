69957e81330a44463e461bc51e097cf95224bf30
xposed01 <43813249+xposed01@users.noreply.github.com>
2022-02-19 23:47:44 +0700

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let numberToString = (Math.abs(n)).toString();
+  let result = '';
+
+  for (let i = 0; i < numberToString.length; i++) {
+    result = `${numberToString[i]}${result}`;
+  }
+  return Number(result);
 }
